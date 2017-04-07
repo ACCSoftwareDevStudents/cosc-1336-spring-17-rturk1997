bae1d01a47f45a2bd755cb9ee4c5ca7d0d1730d7
Robert "Max" Turk <robert.turk@g.austincc.edu>
2017-04-07 14:40:46 -0500

    Add files via upload
    source code for Term project, initial upload

---
+++ enrollment.py
@@ -0,0 +1,12 @@
+from course import Course
+from student import Student
+
+class Enrollment():
+    def __init__(self, enroll_id, student_id, course_id):
+        self.enroll_id = enroll_id
+        self.student_id = student_id 
+        self.course_id = course_id
+        
+
+
+
